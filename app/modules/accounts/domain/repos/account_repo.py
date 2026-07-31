99b52c1a075272c9148f26506069a8d1a8328838
Egore000 <egor.pakhomov.2002@gmail.com>
2026-07-31 21:44:24 +0800

    Репозиторий для работы с моделью Account

---
+++ app/modules/accounts/domain/repos/account_repo.py
@@ -0,0 +1,19 @@
+from typing import Protocol
+from uuid import UUID
+
+from app.modules.accounts.domain.entities.account import Account
+from app.modules.accounts.domain.value_objects import Email
+
+
+class AccountRepository(Protocol):
+    """Репозиторий для работы c сущностью Account"""
+
+    async def add(self, account: Account) -> None: ...
+
+    async def get_by_id(self, account_id: UUID) -> Account | None: ...
+
+    async def get_by_email(self, email: Email) -> Account | None: ...
+
+    async def update(self, account: Account) -> None: ...
+
+    async def delete(self, account_id: UUID) -> None: ...
