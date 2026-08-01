3dc4d0c37722ae63bfd55ebfc32671d0cd8e40dd
Egore000 <egor.pakhomov.2002@gmail.com>
2026-08-01 11:54:53 +0800

    feature: Функции доменной сущности account

--- app/modules/accounts/domain/entities/account.py
+++ app/modules/accounts/domain/entities/account.py
@@ -12,3 +12,15 @@ class Account:
     email: Email
     full_name: FullName
     is_active: bool
+
+    def activate(self) -> None:
+        self.is_active = True
+
+    def deactivate(self) -> None:
+        self.is_active = False
+
+    def change_email(self, new_email: Email) -> None:
+        self.email = new_email
+
+    def change_full_name(self, new_full_name: FullName) -> None:
+        self.full_name = new_full_name
